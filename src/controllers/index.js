4597f4ceab988dee695171e537ce37df735ac550
exbinario <miguel.4to.1994@gmail.com>
2020-05-12 13:43:18 -0600

    se reciben los datos del pago por el controlador principal

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,7 +1,19 @@
+const fs = require('fs');
+
 const mainController = {};
 
 mainController.saludo = (req, res) => {
     res.send('hola');
+}
+
+mainController.llenarFormularioPago = async(req, res) => {
+    // console.log(req.body);
+    // console.log(req.file.filename);
+    
+    res.json({
+        response: 'ok',
+        file: req.file.filename
+    });
 }
 
 module.exports = mainController;
--- src/index.js
+++ src/index.js
@@ -2,15 +2,32 @@ const express = require('express');
 const cors = require('cors');
 const listaRutas = require('express-list-endpoints');
 
+const path = require('path');
+const multer = require('multer');
+const { v4: uuid } = require('uuid');
+
 const app = express();
 
 app.use(cors());
 app.set('port', process.env.PORT || 3000);
 app.use(express.json({extended: true}));
 
+const storage = multer.diskStorage({
+    destination: path.join(__dirname, 'files/pdf/uploads'),
+    filename: (req, file, cb, filename) => {
+        // console.log(file);
+        cb(null, uuid() + path.extname(file.originalname));
+    }
+}) 
+app.use(multer({storage}).single('boletaPago'));
+
+
 const routes = require('./routes/index');
 
 app.use('/', routes);
+
+app.use(express.static(path.join(__dirname, 'files/pdf/uploads')));
+
 
 app.listen(app.get('port'), () => {
     console.log(listaRutas(app));
