9203a56d2013f7dd07d5d30f00b01cbf209b5ce2
exbinario <miguel.4to.1994@gmail.com>
2020-05-14 16:25:47 -0600

    se agregaron los certificados ssl

--- functions/index.js
+++ functions/index.js
@@ -1,17 +1,22 @@
 const functions = require('firebase-functions');
 const express = require('express');
 const cors = require('cors');
-const listaRutas = require('express-list-endpoints');
+// const listaRutas = require('express-list-endpoints');
 
 const path = require('path');
 const multer = require('multer');
 const { v4: uuid } = require('uuid');
+var bodyParser = require('body-parser')
+
 
 const app = express();
 
 app.use(cors());
-app.set('port', process.env.PORT || 3000);
-app.use(express.json({extended: true}));
+// app.set('port', process.env.PORT || 3000);
+app.use(express.urlencoded({extended: true}));
+app.use(express.json({extended: false}));
+
+
 
 const storage = multer.diskStorage({
     destination: path.join(__dirname, 'files/pdf/uploads'),
@@ -22,11 +27,20 @@ const storage = multer.diskStorage({
 }) 
 app.use(multer({storage}).single('boletaPago'));
 
+// app.all("/*", function (req, res, next) {
+//     res.header('Access-Control-Allow-Origin', '*');
+//     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+//     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+//     next();
+//   });
+  
 
 const routes = require('./routes/index');
 
 app.use('/', routes);
 
-app.use(express.static(path.join(__dirname, 'files/pdf/uploads')));
+// app.use(express.static(path.join(__dirname, 'files/pdf/uploads')));
+app.use('/', express.static(path.join(__dirname, 'files/pdf/uploads')));
+
 
 exports.app = functions.https.onRequest(app);
--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const cors = require('cors');
 const listaRutas = require('express-list-endpoints');
-
 const path = require('path');
 const multer = require('multer');
 const { v4: uuid } = require('uuid');
+
+var fs = require('fs');
+var http = require('http');
+var https = require('https');
+var privateKey  = fs.readFileSync('sslcert/privkey.pem', 'utf8');
+var certificate = fs.readFileSync('sslcert/cert.pem', 'utf8');
+
+var credentials = {key: privateKey, cert: certificate};
+
 
 const app = express();
 
@@ -29,7 +37,13 @@ app.use('/', routes);
 app.use(express.static(path.join(__dirname, 'files/pdf/uploads')));
 
 
-app.listen(app.get('port'), () => {
-    console.log(listaRutas(app));
-    console.log('server on port', app.get('port'));
-})
+// app.listen(app.get('port'), () => {
+//     console.log(listaRutas(app));
+//     console.log('server on port', app.get('port'));
+// })
+
+var httpServer = http.createServer(app);
+var httpsServer = https.createServer(credentials, app);
+
+httpServer.listen(80);
+httpsServer.listen(443);
